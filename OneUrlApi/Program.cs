9fd7d6fc6a2b898910f36875c0e1a658bafd430f
Timothy0508 <timothytseng508@gmail.com>
2025-09-07 00:52:53 +0800

    feat: configure doc endpoint

--- OneUrlApi/Program.cs
+++ OneUrlApi/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference("/docs");
 }
 
 app.UseHttpsRedirection();
