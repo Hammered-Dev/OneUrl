ddaf62583c93394d4efef1acbd23f7016f59aba1
Timothy0508 <timothytseng508@gmail.com>
2025-09-07 00:35:28 +0800

    feat: add datatype declaration

--- OneUrlApi/Api/Manage/Manage.cs
+++ OneUrlApi/Api/Manage/Manage.cs
@@ -7,9 +7,15 @@ public static class Manage
         var group = app.MapGroup("/manage");
 
         group.MapGet("/urls", () => "H");
-        group.MapPost("/urls", () => "H");
-        group.MapPatch("/urls/{id}", (string id) => id);
+        group.MapPost("/urls", (UrlRecord record) => "H");
+        group.MapPatch("/urls/{id}", (string id, UrlRecord record) => id);
         group.MapDelete("/urls/{id}", (string id) => id);
 
     }
+}
+
+class UrlRecord
+{
+    required public string Target { get; set; }
+    required public string Location { get; set; }
 }
