da84510f8fe2dcd9efd4fb1868cd7a8260b1f0b3
Timothy0508 <timothytseng508@gmail.com>
2025-09-26 18:12:08 +0800

    feat: add scalar authentication options

--- OneUrlApi/Program.cs
+++ OneUrlApi/Program.cs
@@ -5,16 +5,16 @@ using OpenIddict.Validation.AspNetCore;
 using Scalar.AspNetCore;
 
 DotNetEnv.Env.Load();
-Console.WriteLine(Environment.GetEnvironmentVariable("AUTH_ISSUER")?? "null........");
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddHttpClient();
 
 builder.Services.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+
+builder.Services.AddOpenApi();
 
 builder.Services.AddOpenIddict()
     .AddValidation(options =>
@@ -35,7 +35,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference("/docs");
+    app.MapScalarApiReference("/docs", options =>
+    {
+        options.AddPreferredSecuritySchemes("OAuth2")
+            .AddAuthorizationCodeFlow("OAuth2", flow =>
+            {
+                flow.ClientId = "console";
+                flow.SelectedScopes = ["email", "openid", "profile"];
+            });
+    });
 }
 
 app.UseHttpsRedirection();
