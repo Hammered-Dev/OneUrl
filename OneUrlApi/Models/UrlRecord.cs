a5c47a4d48c28243a58ae6ad585dbbc4121f1d76
Timothy0508 <timothytseng508@gmail.com>
2025-10-29 10:45:32 +0800

    feat: add database intergration

--- OneUrlApi/Models/UrlRecord.cs
+++ OneUrlApi/Models/UrlRecord.cs
@@ -1,3 +1,4 @@
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 
 namespace OneUrlApi.Models;
@@ -9,12 +10,13 @@ public class UrlRecord : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        Env.Load();
         var host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
         var port = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432";
         var database = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "postgres";
         var user = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "postgres";
         var password = Environment.GetEnvironmentVariable("POSTGRES_PASS");
-        optionsBuilder.UseNpgsql($"Host={host}:{port};User={user};Password={password};Database={database}");
+        optionsBuilder.UseNpgsql($"Host={host}:{port};Username={user};Password={password};Database={database}");
         base.OnConfiguring(optionsBuilder);
     }
 
